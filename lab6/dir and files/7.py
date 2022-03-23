a18d0b708f21b084721329d2550fb599668f7642
szholbarys <sakenzholbarys@gmail.com>
2022-03-24 00:47:35 +0500

    main

--- lab6/dir and files/7.py
+++ lab6/dir and files/7.py
@@ -1,8 +1,8 @@
-path1 = "D:\Educational materials\Programming principles 2\Python\PP2_21B030956\Lab6\Dir and files\practice\copy1.txt"
+path1 = r"C:\Users\user\Desktop\pp2.lab\PP2_21B030907\lab6\dir and files\practice1.txt"
 with open(path1, "r") as readd:
     read = readd.read()
     
-path2 = "D:\Educational materials\Programming principles 2\Python\PP2_21B030956\Lab6\Dir and files\practice\copy2.txt"
+path2 = r"C:\Users\user\Desktop\pp2.lab\PP2_21B030907\lab6\dir and files\practice2.txt"
 with open(path2, "w") as write:
     for i in read:
         write.write(i)
